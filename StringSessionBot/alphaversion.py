1b28a5a0d90a9e13307b981b4b5edb195dc36a8d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 00:46:05 +0530

    Update alphaversion.py

--- StringSessionBot/alphaversion.py
+++ StringSessionBot/alphaversion.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 
 @Client.on_message(filters.private & filters.incoming & filters.command("alphaversion"))
-async def alphaversion(bot, msg):
+async def _alphaversion(bot, msg):
     await bot.send_message(
         msg.chat.id,
         Data.ALPHAVERSION,
