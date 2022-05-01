0a299ec4dd38aab939ab72b3b26916fb410fbd0e
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 00:35:09 +0530

    Create alphaversion.py

---
+++ StringSessionBot/alphaversion.py
@@ -0,0 +1,13 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
+
+
+@Client.on_message(filters.private & filters.incoming & filters.command("alphaversion"))
+async def alphaversion(bot, msg):
+    await bot.send_message(
+        msg.chat.id,
+        Data.ALPHAVERSION,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
+    )
