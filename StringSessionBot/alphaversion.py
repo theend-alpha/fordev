d05f23988e63fa8ddaf5c6e28d87906c915e02d5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 08:23:30 +0530

    Update alphaversion.py

--- StringSessionBot/alphaversion.py
+++ StringSessionBot/alphaversion.py
@@ -1,4 +1,4 @@
-from Yashvi import Data
+from Yashvi import Keshav
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
 
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup
 async def _alphaversion(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        Data.ALPHAVERSION,
+        Keshav.ALPHAVERSION,
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
+        reply_markup=InlineKeyboardMarkup(Keshav.home_buttons),
     )
