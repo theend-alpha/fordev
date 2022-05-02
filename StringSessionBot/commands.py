27a46dfb64fd95be73cc51d7e8a629db9442e495
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 07:32:08 +0530

    Update commands.py

--- StringSessionBot/commands.py
+++ StringSessionBot/commands.py
@@ -3,11 +3,11 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command("commands"))
-async def _commands(bot, msg):
+@Client.on_message(filters.private & filters.incoming & filters.command("cmda"))
+async def _cmda(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        Data.COMMANDS,
+        Data.CMDA,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons),
     )
