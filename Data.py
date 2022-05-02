da934dc1ad3cb467fcab41364065a3d4a251ff5d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 07:44:45 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -73,3 +73,38 @@ Language Used : Python
            
 Contact Owner and Developers [here](https://t.me/BTS_CHAT_ZONE) 
 """
+
+# Commands
+   COMMANDS = """
+ Available commands in Alpha Bot 
+
+/start    - To start the bot ✨💫
+/generate - To start string generation !
+/help     - To view the tutorial.
+/about    - Details to contact the developer !
+
+"""
+
+    # version
+    ALPHAVERSION = """
+ Alpha Version 
+
+$ Version Name        - end.2.0
+$ Version released on - 01/05/2022
+$ Updated by          - [Alpha](t.me/NotReallyAlpha)
+
+ Updated features 
+
+$ Added "commands" button for new users !
+$ Added "Alpha Version" button !
+$ Bug fixes 
+
+ Upcoming update 
+
+$ you can see next update on 15/05/2022 !
+$ going to add cool pic : when the user starts the bot !
+
+---
+$ If any suggestions --> [Alpha](t.me/NotReallyAlpha)
+
+"""
