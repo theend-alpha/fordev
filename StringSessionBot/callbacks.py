11ecac4ac96957c71b038ea824754d702127fcb5
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 14:44:42 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -52,7 +52,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
          )
-     elif query == "alphaversion":
+    elif query == "alphaversion":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
