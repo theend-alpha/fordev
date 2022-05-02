7294aa1fee011521b4191e758cc151104e8598b8
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-02 07:42:28 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -41,13 +41,13 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
-    elif query == "commands":
+    elif query == "cmda":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.message_id
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text=Data.COMMANDS,
+            text=Data.CMDA,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
          )
